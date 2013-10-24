26813f869f157bb1917dcf05380ae969b0be13a8
Bartosz Pranczke <bartosz.pranczke@netguru.pl>
2013-10-24 10:26:39 +0200

    Add colors

--- gem_version.sh
+++ gem_version.sh
@@ -6,6 +6,7 @@ do
   version=`echo $line | cut -d '(' -f2 | sed 's/.$//'`
   latest_version=`curl -s https://rubygems.org/api/v1/versions/$gem.json | egrep -oh 'number":"(\w|\.)*"' | head -n1 | cut -d ":" -f2 | sed 's/"//g'`
   if [ "$version" != "$latest_version" ]; then
-    echo "You're using: $gem $version. The latest version is $latest_version"
+    echo "You're using: \x1B[00;34m$gem\x1B[00m\x1B[00;31m$version\x1B[00m. The latest version is \x1B[00;32m$latest_version\x1B[00m"
   fi
-done  
+done
+
